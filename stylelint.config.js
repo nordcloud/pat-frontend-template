1ffa8afd93dd92404fe71820e29c90d07a799ff6
nowyDEV <dominik.nowik@nordcloud.com>
2021-12-15 17:04:29 +0100

    fix css linting

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,5 @@
 /* Copyright (c) 2021 Nordcloud Oy or its affiliates. All Rights Reserved. */
 
 module.exports = {
-  customSyntax: "@stylelint/postcss-css-in-js",
   extends: ["@nordcloud/eslint-config-pat/stylelint.config.js"],
 };
