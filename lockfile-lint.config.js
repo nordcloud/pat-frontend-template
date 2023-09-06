158d8d30808d48756501aa61611e8b180c5df46d
Dominik Nowik <12304307+nowyDEV@users.noreply.github.com>
2023-09-06 12:36:47 +0200

    Add lockfile linter (#28)
    Co-authored-by: nowyDEV <nowyDEV@users.noreply.github.com>

---
+++ lockfile-lint.config.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2023 Nordcloud Oy or its affiliates. All Rights Reserved.
+ */
+
+/** @see https://github.com/lirantal/lockfile-lint/tree/main/packages/lockfile-lint#file-based-configuration */
+module.exports = {
+  type: "npm",
+  path: "package-lock.json",
+  allowedHosts: "npm",
+  validateHttps: true,
+  validateIntegrity: false,
+  validatePackageNames: true,
+  allowedPackageNameAliases: [
+    "string-width-cjs:string-width",
+    "strip-ansi-cjs: strip-ansi",
+    "wrap-ansi-cjs:wrap-ansi",
+  ],
+};
