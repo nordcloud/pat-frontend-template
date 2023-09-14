9950b044977e5b31e6db2696b2bb9d92697e47fb
nowyDEV <dominik.nowik@nordcloud.com>
2023-09-14 15:36:39 +0200

    fix types of jest matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,16 +1,8 @@
 /* Copyright (c) 2021-2023 Nordcloud Oy or its affiliates. All Rights Reserved. */
 
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { configure } from "@testing-library/react";
 import { expect, vi } from "vitest";
-
-declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- it makes sense to have it here
-  type Assertion<T = any> = jest.Matchers<void, T> &
-    TestingLibraryMatchers<T, void> &
-    object;
-}
 
 expect.extend(matchers);
 
